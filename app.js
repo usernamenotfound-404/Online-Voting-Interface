06cdb2892f1d2f951a682cfbfa5e51498c9a9468
Zenith <131651367+usernamenotfound-404@users.noreply.github.com>
2023-06-17 17:16:22 +0530

    Added basic voting functions

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-alert("Working well")
+var candidates=[1,2,3]
+var votes=[]
+
+var voted=false;
+
+var state=null;
+
+function createVoteList(candidates,votes){
+    for(i=0;i<candidates.length;i++){
+        votes[i]=0;
+    }
+}
+createVoteList(candidates,votes);
+
+function updateVoteCount(candidate, count) {
+    const voteCountElement = document.getElementById(`${candidate}-votes`);
+    if (voteCountElement) {
+        voteCountElement.innerText = count;
+    }
+}
+
+function vote(){
+    for(var i=1;i<(candidates.length+1);i++){
+        //console.log(i)
+        if((document.getElementById(`candidate${i}`).checked)==true){
+            console.log(candidates[i-1])
+            votes[i-1]+=1;
+        }
+    }
+    console.log(votes)
+}
