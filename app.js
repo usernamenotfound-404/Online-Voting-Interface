4650cae1e867c79fabdd404b1d9b761786e83316
Zenith <131651367+usernamenotfound-404@users.noreply.github.com>
2023-06-20 17:42:49 +0530

    .

--- app.js
+++ app.js
@@ -13,19 +13,24 @@ function createVoteList(candidates,votes){
 createVoteList(candidates,votes);
 
 function updateVoteCount(candidate, count) {
-    const voteCountElement = document.getElementById(`${candidate}-votes`);
-    if (voteCountElement) {
-        voteCountElement.innerText = count;
+    const voteCountElement = document.getElementById(`candidate1-votes`);
+
+    if (voteCountElement!=null && voteCountElement!=undefined) {
+        voteCountElement.innerText = votes[1];
     }
 }
 
 function vote(){
     for(var i=1;i<(candidates.length+1);i++){
-        //console.log(i)
         if((document.getElementById(`candidate${i}`).checked)==true){
-            console.log(candidates[i-1])
             votes[i-1]+=1;
         }
+
+        const voteCountElement = document.getElementById(`candidate${i}-votes`);
+        if (voteCountElement!=null && voteCountElement!=undefined) {
+            voteCountElement.innerText = votes[i-1];
+        }
+
     }
     console.log(votes)
 }
